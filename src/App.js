17bc3f34be04a6f7849443eab9d9a6b7669091a7
Bo Latt <bolattny@gmail.com>
2023-07-15 18:30:37 -0400

    remove strict mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  console.log("<App /> rendederd");
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("http://localhost:8888");
+      console.log(response);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <h1>hello world</h1>
