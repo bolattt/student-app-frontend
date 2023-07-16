b3f2aa67adc8e347967ea81325ed97ae8e149394
Bo Latt <bolattny@gmail.com>
2023-07-16 02:10:41 -0400

    add search functionality

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./SearchBar.scss";
-export default function SearchBar() {
+export default function SearchBar({ search, setSearch }) {
+  console.log("<SearchBar /> rendederd");
   return (
     <div className="searchBar">
-      <input type="text" placeholder="Search by name" />
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </div>
   );
 }
--- src/components/studentList/StudentList.jsx
+++ src/components/studentList/StudentList.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import StudentCard from "../studentCard/StudentCard";
 import SearchBar from "../searchBar/SearchBar";
 import "./StudentList.scss";
 
 export default function StudentList({ students }) {
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    function handleSearch() {
+      setSearchResults(
+        students.filter(
+          (student) =>
+            student.firstName.toLowerCase().includes(search.toLowerCase()) ||
+            student.lastName.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+    handleSearch();
+  }, [search]);
+
+  console.log("searchREsult", searchResults);
+
   console.log("<StudentList /> rendederd");
   return (
     <div className="studentList">
-      <SearchBar />
-      {students.map((student) => (
-        <StudentCard key={student.id} student={student} />
-      ))}
+      <SearchBar search={search} setSearch={setSearch} />
+      {search != ""
+        ? searchResults.map((student) => (
+            <StudentCard key={student.id} student={student} />
+          ))
+        : students.map((student) => (
+            <StudentCard key={student.id} student={student} />
+          ))}
     </div>
   );
 }
