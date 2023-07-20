4cb63285b990aa96ac0de7244c33234948d49c8a
Bo Latt <bolattny@gmail.com>
2023-07-19 20:39:24 -0400

    add search bar

--- src/components/studentCard/StudentCard.jsx
+++ src/components/studentCard/StudentCard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { useState } from "react";
+import plus from "../../assets/plus-solid.svg";
+import minus from "../../assets/minus-solid.svg";
 import "./StudentCard.scss";
+
 export default function StudentCard({ student }) {
   console.log("<StudentCard /> rendederd");
   const { company, email, firstName, lastName, grades, pic, skill } = student;
-
+  const [showGrades, setShowGrades] = useState(false);
   function getAverageGrade(grades) {
     let total = 0;
     for (let i = 0; i < grades.length; i++) {
@@ -19,22 +22,31 @@ export default function StudentCard({ student }) {
         <img className="" src={pic} alt={firstName + " " + lastName} />
       </div>
       <div className="studentCard__info">
-        <h1 className="studentCard__name">
-          {firstName} {lastName}
-        </h1>
+        <div className="studentCard__nameWrapper">
+          <h1 className="studentCard__name">
+            {firstName} {lastName}
+          </h1>
+          <button
+            className="studentCard__toggle"
+            onClick={() => setShowGrades(!showGrades)}
+          >
+            <img src={showGrades ? minus : plus} alt="toggle" />
+          </button>
+        </div>
+
         <p className="studentCard__email">Email: {email}</p>
         <p className="studentCard__company">Company: {company}</p>
         <p className="studentCard__skills">Skill: {skill}</p>
         <p className="studentCard__averageGrade">Average: {averageGrade}%</p>
-        {/* <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-          vitae tenetur. Facilis autem ratione dolorum assumenda iusto aliquam,
-          impedit harum repellendus iure id ullam ab debitis natus unde
-          molestiae ipsa. Lorem ipsum dolor, sit amet consectetur adipisicing
-          elit. Libero quo ipsam modi possimus reiciendis minus nobis suscipit,
-          vitae vero ducimus quaerat nostrum repudiandae consequuntur error
-          mollitia aut tenetur, incidunt eligendi.
-        </p> */}
+        <div className="studentCard__grades">
+          <br />
+          {showGrades &&
+            grades.map((grade, index) => (
+              <p key={index} className="studentCard__grade">
+                Test {index + 1}: &nbsp; &nbsp; &nbsp; {grade}%
+              </p>
+            ))}
+        </div>
       </div>
     </div>
   );
